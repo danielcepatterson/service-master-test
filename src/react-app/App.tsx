4fbe9fa0f65e42ab41e3e3ba70e1190bd586587d
danielcepatterson <danielcepatterson@gmail.com>
2025-12-27 00:08:52 -0500

    Refactor App component to include CSS import and improve structure

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,3 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", minHeight: "100vh" }}>
+			<h1>Welcome to the Service Master App</h1>
+			<p>This is your splash page. Start building your application here!</p>
+		</div>
+	);
+}
+
+export default App;
 
 import React from "react";
 import "./App.css";
